64740b32e25df4c22161ed75e7890250606432bd
dussn <dustin.heuser@gmail.com>
2021-04-14 16:42:12 -0400

    calendar fix

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,23 +3,30 @@ import "./App.css";
 import React from "react";
 import Auth from './Login/auth';
 import Cookies  from 'universal-cookie';
-
+import logo from './Imgs/test.PNG';
 
 function HomeInfo() {
   return( 
-    <div class="container-fluid">
-	    <div class="row">
-		    <div class="col-md-12">
-			    <div class="jumbotron">
-				    <h2>
-					    About Us
-				    </h2>
-				    <p>
-					  Florida TrailGator is a group that promotes physical activity while discovering the local nature around Gainesville. We go on trail walks/runs, hikes, and have local nature outings.
-				    </p>
+    <div class = 'home-container'>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="jumbotron">
+              <h2>
+                About Us
+              </h2>
+              <p>
+              Florida TrailGator is a group that promotes physical activity while discovering the local nature around Gainesville. We go on trail walks/runs, hikes, and have local nature outings.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+    </div>
+    <div class="container-fluid">
+      <img class="card-img-top" alt="logo" src={logo} />
+    </div>
+   
+
   </div>
 
   )
--- client/src/User/calendar.js
+++ client/src/User/calendar.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import React from "react";
+import "./calendar.css";
+import Calendar from "@ericz1803/react-google-calendar";
+import { css } from "@emotion/react";
 
-export default class EventCalendar extends React.Component {
-  render() {
-    return (
-        <div>
+//put your google calendar api key here
+const API_KEY = "AIzaSyDk4csiNvSTyVQE3aMZOZFJpqHuCasyqUg";
+
+//replace calendar id with one you want to test
+
+let calendars = [
+  { calendarId: "c_4hamaug2ns0tcivss6kg3hk2ug@group.calendar.google.com" },
+  { calendarId: "en.usa#holiday@group.v.calendar.google.com" },
+  {
+    calendarId: "ncaaf_67_%46lorida+%47ators#sports@group.v.calendar.google.com"
+  }
+];
+
+let styles = {
+  //you can use object styles
+  calendar: {
+    borderWidth: "3px" //make outer edge of calendar thicker
+  },
+
+  //you can also use emotion's string styles (remember to add the line 'import { css } from "@emotion/react";')
+  today: css`
+    /* highlight today by making the text red and giving it a red border */
+    color: red;
+    border: 3px solid red;
+  `
+};
+
+export default function EventCalendar() {
+  return (
+      
+    <div className="App">
+    <div>
             <nav>
                 <div className="form-check form-check-inline nowrap">
                     <a className="nav-link" href="/"> Home</a> |
@@ -16,12 +43,22 @@ export default class EventCalendar extends React.Component {
                 </div>
         
              </nav>
-            <iframe src="https://calendar.google.com/calendar/embed?src=c_4hamaug2ns0tcivss6kg3hk2ug%40group.calendar.google.com&ctz=America%2FNew_York"  width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
         </div>
-   
-    )}
-  
+      <body>
+        <div
+          style={{
+            width: "90%",
+            paddingTop: "50px",
+            paddingBottom: "50px",
+            margin: "auto",
+            maxWidth: "1200px"
+          }}
+        >
+          <Calendar apiKey={API_KEY} calendars={calendars} styles={styles} />
+        </div>
+      </body>
+    </div>
+  );
 }
 
 
